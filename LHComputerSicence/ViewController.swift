42a0d1e79e6999920634984f9af713575fd3ebd4
李辉 <lihui@mucang.cn>
2019-06-08 09:18:33 +0800

    工程实现改为swift语言

---
+++ LHComputerSicence/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LHComputerSicence
+//
+//  Created by lihui on 2019/6/8.
+//  Copyright © 2019 Lihux. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
