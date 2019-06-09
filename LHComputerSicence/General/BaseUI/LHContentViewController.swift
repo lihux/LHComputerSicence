906d4e06cbfbd405c94c13f08292a2a4a58de059
李辉 <lihui@mucang.cn>
2019-06-09 16:24:49 +0800

    添加APP icon 1.0版本

---
+++ LHComputerSicence/General/BaseUI/LHContentViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LHContentViewController.swift
+//  LHComputerSicence
+//
+//  Created by lihui on 2019/6/8.
+//  Copyright © 2019 Lihux. All rights reserved.
+//
+
+import UIKit
+
+class LHContentViewController: LHViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ LHComputerSicence/General/BaseUI/LHHomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LHHomeViewController.swift
+//  LHComputerSicence
+//
+//  Created by lihui on 2019/6/8.
+//  Copyright © 2019 Lihux. All rights reserved.
+//
+
+import UIKit
+
+class LHHomeViewController: LHViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
